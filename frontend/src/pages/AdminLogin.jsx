a19b28d34e60c81515330d4659024e0561d57107
KIJ <dlrwn0730@naver.com>
2025-08-26 10:45:42 +0900

    .

---
+++ frontend/src/pages/AdminLogin.jsx
@@ -0,0 +1,79 @@
+import React, { useEffect, useState } from 'react'
+import { api } from '../lib/api'
+import { useNavigate } from 'react-router-dom'
+
+
+const AdminLogin = () => {
+
+    const nav = useNavigate()
+    const [formData, setFormData] = useState({
+        username: '',
+        password: ''
+    })
+
+    const [checking, setChecking] = useState(true)
+    const [submitting, setSubmitting] = useState(false)
+
+    const [error, setError] = useState(null)
+
+    const handleChange = (e) => {
+        setFormData({
+            ...formData,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+
+        try {
+            const response = await api.post('/api/auth/login',
+                formData, {
+                withCredentials: true
+            }
+            )
+
+            if (response.data.user) {
+                nav('/admin/post')
+            }
+
+        } catch (error) {
+            const errorMsg = 
+        }
+    }
+
+    return (
+        <div>
+            <div className='login-header'>
+                <h3>관리자 로그인</h3>
+                <p>관리자 전용 페이지 입니다.</p>
+            </div>
+            <form className='login-form'>
+                <div className="form-field">
+                    <label htmlFor="username">관리자 아이디 : </label>
+                    <input
+                        type="text"
+                        id='username'
+                        required placeholder='관리자 아이디'
+                        onChange={handleChange}
+                        value={formData.username}
+                    />
+                </div>
+                <div className="form-field">
+                    <label htmlFor="password">관리자 비밀번호 : </label>
+                    <input
+                        type="password"
+                        id='password' required
+                        placeholder='관리자 비밀번호'
+                        onChange={handleChange}
+                        value={formData.password}
+                    />
+                </div>
+                <div className="error-box"></div>
+                <button type='submit'>로그인</button>
+            </form>
+        </div>
+    )
+}
+
+export default AdminLogin
---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import Hero from '../components/Hero'
+import Aboutme from '../components/Aboutme'
+import Header from '../components/Header'
+import Work from '../components/Work'
+import Contact from '../components/Contact'
+import Footer from '../components/Footer'
+
+
+const Home = () => {
+    return (
+        <div>
+            <Header />
+            <Hero />
+            <Work />
+            <Contact />
+            <Aboutme />
+            <Footer />
+        </div>
+    )
+}
+
+export default Home
