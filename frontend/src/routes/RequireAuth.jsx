05ee550da79ea5b343dbe7fc6f16395fc1916de5
KIJ <dlrwn0730@naver.com>
2025-08-27 10:05:09 +0900

    .

---
+++ frontend/src/routes/RequireAuth.jsx
@@ -0,0 +1,8 @@
+import React, { useEffect, useState } from 'react'
+import { Navigate } from 'react-router-dom'
+import { api } from '../lib/api'
+export default function RequireAuth() {
+    return (
+        <div>RequireAuth</div>
+    )
+}
